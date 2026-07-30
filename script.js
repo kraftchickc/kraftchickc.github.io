9379115c1380339209c7acb43ad804c8c7bd9de4
kraftchickc <kraftchickc@gmail.com>
2026-07-30 15:01:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-document.getElementById('tb-date').textContent = new Date().toLocaleDateString('en-US', {
-  year: 'numeric',
-  month: '2-digit',
-  day: '2-digit'
-});
+const dateEl = document.getElementById('sb-date');
+if (dateEl) {
+  dateEl.textContent = new Date().toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  });
+}
