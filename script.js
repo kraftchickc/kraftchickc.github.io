0d9a7acd32eebae4d6e57b6b9117f8f2222c2d0f
kraftchickc <kraftchickc@gmail.com>
2026-07-30 15:11:42 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const dateEl = document.getElementById('sb-date');
-if (dateEl) {
-  dateEl.textContent = new Date().toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit'
-  });
-}
